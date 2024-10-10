ce733c22c8966a2aa2fa766c942dcb56e10f8b57
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-10-10 15:15:06 +0800

    add div enclosing image and content part

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,28 +13,31 @@ export default function Card() {
 
   return (
     <div className={styles.container}>
-      <img
-        src={decoration}
-        alt='Green dresser with decorative items'
-        className={styles.image}
-      />
-      <div className={styles.content}>
-        <h1 className={styles.title}>
-          Shift the overall look and feel by adding these wonderful touches to
-          furniture in your home
-        </h1>
-        <p className={styles.description}>
-          Ever been in a room and felt like something was missing? Perhaps it
-          felt slightly bare and uninviting. I've got some simple tips to help
-          you make any room feel complete.
-        </p>
-
+      <div className={styles.imageContainer}>
+        <img
+          src={decoration}
+          alt='Green dresser with decorative items'
+          className={styles.image}
+        />
       </div>
-      {isActive ? (
-        <ActiveSection toggleActive={toggleActive} />
-      ) : (
-        <Profile toggleActive={toggleActive} />
-      )}
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>
+            Shift the overall look and feel by adding these wonderful touches to
+            furniture in your home
+          </h1>
+          <p className={styles.description}>
+            Ever been in a room and felt like something was missing? Perhaps it
+            felt slightly bare and uninviting. I've got some simple tips to help
+            you make any room feel complete.
+          </p>
+        </div>
+        {isActive ? (
+          <ActiveSection toggleActive={toggleActive} />
+        ) : (
+          <Profile toggleActive={toggleActive} />
+        )}
+      </div>
     </div>
   );
 }
