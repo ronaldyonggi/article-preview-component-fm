96b3e4d00ba68358a800414787a553b242502fe9
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-11-01 17:46:59 +0800

    configure vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/article-preview-component-fm/',
-})
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+});
